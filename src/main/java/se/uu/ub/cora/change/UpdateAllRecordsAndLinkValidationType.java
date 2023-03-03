43a064e6a30b4134d0d91542ef6834e786179659
marcus <marcus@893436d41b47>
2023-03-03 08:00:12 +0000

    Add validation type to all records

--- src/main/java/se/uu/ub/cora/change/UpdateAllRecordsAndLinkValidationType.java
+++ src/main/java/se/uu/ub/cora/change/UpdateAllRecordsAndLinkValidationType.java
@@ -1,18 +1,30 @@
 package se.uu.ub.cora.change;
 
+import java.util.List;
+
 import se.uu.ub.cora.clientdata.ClientData;
+import se.uu.ub.cora.clientdata.ClientDataAtomic;
+import se.uu.ub.cora.clientdata.ClientDataGroup;
 import se.uu.ub.cora.clientdata.ClientDataList;
+import se.uu.ub.cora.clientdata.ClientDataProvider;
 import se.uu.ub.cora.clientdata.ClientDataRecord;
+import se.uu.ub.cora.clientdata.ClientDataRecordGroup;
+import se.uu.ub.cora.clientdata.ClientDataRecordLink;
 import se.uu.ub.cora.javaclient.cora.DataClient;
 import se.uu.ub.cora.javaclient.cora.DataClientFactoryImp;
 
 public class UpdateAllRecordsAndLinkValidationType {
 
+	private static final String DEF_TEXT_ID = "defTextId";
+	private static final String TEXT_ID = "textId";
+	private static final String DEF_TEXT = "DefText";
+	private static final String TEXT = "Text";
 	private static final String ID = "id";
 	private static final String DATA_DIVIDER = "dataDivider";
 	private static final String RECORD_INFO = "recordInfo";
 	private DataClientFactoryImp dataClientFactory;
 	private DataClient dataClient;
+	private int count = 0;
 
 	public UpdateAllRecordsAndLinkValidationType(String apptokenUrl, String baseUrl) {
 		dataClientFactory = DataClientFactoryImp.usingAppTokenVerifierUrlAndBaseUrl(apptokenUrl,
@@ -23,29 +35,171 @@ public class UpdateAllRecordsAndLinkValidationType {
 
 	public void updateAllRecords() {
 		ClientDataList listOfRecordTypes = dataClient.readList("recordType");
-		int count = 0;
 
-		for (ClientData data : listOfRecordTypes.getDataList()) {
-			ClientDataRecord dataRecord = (ClientDataRecord) data;
+		for (ClientData recordTypeData : listOfRecordTypes.getDataList()) {
+			ClientDataRecord recordTypeDataRecord = (ClientDataRecord) recordTypeData;
 
-			System.out.println(
-					"Counting type: " + dataRecord.getId() + " partial counting: " + count);
-			// WE SHOULD SKIP ABSTRACT TYPES
-			if (noErrorOnListingTheRecordTypes(dataRecord)) {
-				ClientDataList listRecordsForType = dataClient.readList(dataRecord.getId());
-				for (ClientData recordData : listRecordsForType.getDataList()) {
-					count++;
-				}
-				count++;
+			System.out.println("Listing type: " + recordTypeDataRecord.getId()
+					+ " partial counting: " + count);
+
+			if (allowedRecordType(recordTypeDataRecord)) {
+				updateAllRecordsForRecordType(recordTypeDataRecord);
 			}
+			System.out.println();
 		}
 		System.out.println("Records in the system: " + count);
 	}
 
-	private boolean noErrorOnListingTheRecordTypes(ClientDataRecord dataRecord) {
-		return !dataRecord.getId().equals("sound") && !dataRecord.getId().equals("systemSecret")
-				&& !dataRecord.getId().equals("image") && !dataRecord.getId().equals("appToken")
-				&& !dataRecord.getId().equals("binary");
+	private void updateAllRecordsForRecordType(ClientDataRecord recordTypeDataRecord) {
+		ClientDataList listRecordsForType = dataClient.readList(recordTypeDataRecord.getId());
+		for (ClientData recordData : listRecordsForType.getDataList()) {
+			updateRecord(recordData);
+			count++;
+		}
+	}
+
+	private void updateRecord(ClientData recordData) {
+		ClientDataRecord recordDataRecord = (ClientDataRecord) recordData;
+		String recordTypeId = recordDataRecord.getType();
+		ClientDataRecordGroup dataRecordGroup = recordDataRecord.getDataRecordGroup();
+		ClientDataGroup recordInfo = dataRecordGroup.getFirstGroupWithNameInData("recordInfo");
+
+		if (!recordInfo.containsChildWithNameInData("validationType")) {
+
+			recordInfo.addChild(ClientDataProvider.createRecordLinkUsingNameInDataAndTypeAndId(
+					"validationType", "validationType", recordTypeId));
+			System.out.println("Updating record: " + recordDataRecord.getType() + ":"
+					+ recordDataRecord.getId());
+
+			try {
+
+				dataClient.update(dataRecordGroup.getType(), dataRecordGroup.getId(),
+						dataRecordGroup);
+			} catch (Exception e) {
+				System.out.println("Error: " + recordDataRecord.getType() + ":"
+						+ recordDataRecord.getId() + ":" + e.getMessage());
+			}
+		} else {
+			System.out.println("Skipping record: " + recordDataRecord.getType() + ":"
+					+ recordDataRecord.getId());
+		}
+	}
+
+	private boolean allowedRecordType(ClientDataRecord dataRecord) {
+		return forbiddenRecordTypes(dataRecord)
+				&& isImplementingType(dataRecord.getDataRecordGroup());
+	}
+
+	private boolean isImplementingType(ClientDataRecordGroup recordTypeRecordGroup) {
+		return recordTypeRecordGroup.containsChildWithNameInData("abstract")
+				&& recordTypeRecordGroup.getFirstDataAtomicWithNameInData("abstract").getValue()
+						.equals("false");
+	}
+
+	private boolean forbiddenRecordTypes(ClientDataRecord dataRecord) {
+		return !dataRecord.getId().equals("systemSecret") && !dataRecord.getId().equals("appToken");
+	}
+
+	public void addMissingTextsAndDefTexts() {
+		List<String> recordTypesToLoop = List.of("permissionRole", "permissionRule");
+
+		for (String recordType : recordTypesToLoop) {
+			ClientDataList readList = dataClient.readList(recordType);
+
+			for (ClientData recordTypeData : readList.getDataList()) {
+				ClientDataRecord recordDataRecord = (ClientDataRecord) recordTypeData;
+				ClientDataRecordGroup dataRecordGroup = recordDataRecord.getDataRecordGroup();
+
+				System.out.println("Reading record: " + dataRecordGroup.getType() + ":"
+						+ dataRecordGroup.getId());
+
+				updateText(recordType, dataRecordGroup, TEXT_ID, TEXT);
+				updateText(recordType, dataRecordGroup, DEF_TEXT_ID, DEF_TEXT);
+				try {
+					dataClient.update(recordType, dataRecordGroup.getId(), dataRecordGroup);
+				} catch (Exception e) {
+					System.out.println("Error on record: " + dataRecordGroup.getType() + ":"
+							+ dataRecordGroup.getId());
+				}
+
+			}
+		}
+	}
+
+	private void updateText(String recordType, ClientDataRecordGroup dataRecordGroup,
+			String nameInData, String typeOfText) {
+		if (textMissing(nameInData, dataRecordGroup)) {
+			String textId = createAndStoreDefTextForValidationType(recordType, dataRecordGroup,
+					typeOfText);
+			dataRecordGroup.removeFirstChildWithNameInData(nameInData);
+			ClientDataRecordLink link = ClientDataProvider
+					.createRecordLinkUsingNameInDataAndTypeAndId(nameInData, "coraText", textId);
+			dataRecordGroup.addChild(link);
+			System.out.println("Update " + nameInData + " record: " + dataRecordGroup.getType()
+					+ ":" + dataRecordGroup.getId());
+		}
+	}
+
+	private boolean textMissing(String nameInData, ClientDataRecordGroup dataRecordGroup) {
+		return !dataRecordGroup.containsChildWithNameInData(nameInData);
+	}
+
+	private String createAndStoreDefTextForValidationType(String recordType,
+			ClientDataRecordGroup readRecordTypeRecordGroup, String typeOfText) {
+		ClientDataGroup readRecordInfo = (ClientDataGroup) readRecordTypeRecordGroup
+				.getFirstChildWithNameInData(RECORD_INFO);
+		ClientDataRecordGroup dataRecordGroupText = ClientDataProvider
+				.createRecordGroupUsingNameInData("text");
+		ClientDataGroup recordInfoText = ClientDataProvider.createGroupUsingNameInData(RECORD_INFO);
+		recordInfoText.addChild(copyLink(readRecordInfo, DATA_DIVIDER));
+
+		ClientDataAtomic idText = ClientDataProvider.createAtomicUsingNameInDataAndValue(ID,
+				readRecordTypeRecordGroup.getId() + capitalize(recordType) + typeOfText);
+		recordInfoText.addChild(idText);
+		dataRecordGroupText.addChild(recordInfoText);
+		// sv
+		ClientDataGroup textPartSv = ClientDataProvider.createGroupUsingNameInData("textPart");
+		dataRecordGroupText.addChild(textPartSv);
+		textPartSv.addAttributeByIdWithValue("type", "default");
+		textPartSv.addAttributeByIdWithValue("lang", "sv");
+		ClientDataAtomic defTextSv = ClientDataProvider.createAtomicUsingNameInDataAndValue("text",
+				"Text för " + readRecordTypeRecordGroup.getId());
+		textPartSv.addChild(defTextSv);
+		// en
+		ClientDataGroup textPartEn = ClientDataProvider.createGroupUsingNameInData("textPart");
+		dataRecordGroupText.addChild(textPartEn);
+		textPartEn.addAttributeByIdWithValue("type", "alternative");
+		textPartEn.addAttributeByIdWithValue("lang", "en");
+		ClientDataAtomic defTextEn = ClientDataProvider.createAtomicUsingNameInDataAndValue("text",
+				"Text for " + readRecordTypeRecordGroup.getId());
+		textPartEn.addChild(defTextEn);
+
+		ClientDataRecord create = dataClient.create("coraText", dataRecordGroupText);
+		return create.getId();
+	}
+
+	private String capitalize(String recordType) {
+		// TODO Auto-generated method stub
+		return recordType.substring(0, 1).toUpperCase() + recordType.substring(1);
+	}
+
+	private ClientDataRecordLink getLinkIdByNameInData(ClientDataGroup recordTypeRecordGroup,
+			String linkNameInData) {
+		return (ClientDataRecordLink) recordTypeRecordGroup
+				.getFirstChildWithNameInData(linkNameInData);
+	}
+
+	private ClientDataRecordLink copyLink(ClientDataGroup recordTypeRecordGroup,
+			String nameInData) {
+		ClientDataRecordLink link = getLinkIdByNameInData(recordTypeRecordGroup, nameInData);
+		return createLink(nameInData, link.getLinkedRecordType(), link.getLinkedRecordId());
+	}
+
+	private ClientDataRecordLink createLink(String nameInData, String linkType, String linkId) {
+
+		return ClientDataProvider.createRecordLinkUsingNameInDataAndTypeAndId(nameInData, linkType,
+				linkId);
+
 	}
 
 }
--- src/test/java/se/uu/ub/cora/change/UpdateAllRecordsAndLinkValidationTypeTest.java
+++ src/test/java/se/uu/ub/cora/change/UpdateAllRecordsAndLinkValidationTypeTest.java
@@ -4,14 +4,14 @@ import org.testng.annotations.Test;
 
 public class UpdateAllRecordsAndLinkValidationTypeTest {
 
-	private static final String SYSTEMONE_UTV_APPTOKEN_URL = "http://localhost:8180/apptokenverifier/rest/";
-	private static final String SYSTEMONE_UTV_BASE_URL = "http://localhost:8080/systemone/rest/";
+	private static final String SYSTEMONE_LOCAL_APPTOKEN_URL = "http://localhost:8180/apptokenverifier/rest/";
+	private static final String SYSTEMONE_LOCAL_BASE_URL = "http://localhost:8080/systemone/rest/";
 
-	private static final String ALVIN_UTV_APPTOKEN_URL = "http://localhost:8181/apptokenverifier/rest/";
-	private static final String ALVIN_UTV_BASE_URL = "http://localhost:8081/alvin/rest/";
+	private static final String ALVIN_LOCAL_APPTOKEN_URL = "http://localhost:8181/apptokenverifier/rest/";
+	private static final String ALVIN_LOCAL_BASE_URL = "http://localhost:8081/alvin/rest/";
 
-	private static final String DIVA_UTV_APPTOKEN_URL = "http://localhost:8182/apptokenverifier/rest/";
-	private static final String DIVA_UTV_BASE_URL = "http://localhost:8082/diva/rest/";
+	private static final String DIVA_LOCAL_APPTOKEN_URL = "http://localhost:8182/apptokenverifier/rest/";
+	private static final String DIVA_LOCAL_BASE_URL = "http://localhost:8082/diva/rest/";
 
 	private static final String SYSTEMONE_DEV_APPTOKEN_URL = "http://130.238.171.238:38180/apptokenverifier/rest/";
 	private static final String SYSTEMONE_DEV_BASE_URL = "http://130.238.171.238:38080/systemone/rest/";
@@ -23,12 +23,48 @@ public class UpdateAllRecordsAndLinkValidationTypeTest {
 	private static final String DIVA_DEV_BASE_URL = "http://130.238.171.238:38082/diva/rest/";
 
 	@Test(enabled = false)
-	public void testName() throws Exception {
+	public void runLocalSystemOne() throws Exception {
 
 		UpdateAllRecordsAndLinkValidationType updater = new UpdateAllRecordsAndLinkValidationType(
-				SYSTEMONE_UTV_APPTOKEN_URL, SYSTEMONE_UTV_BASE_URL);
+				SYSTEMONE_LOCAL_APPTOKEN_URL, SYSTEMONE_LOCAL_BASE_URL);
 
 		updater.updateAllRecords();
+	}
+
+	@Test(enabled = false)
+	public void runLocalAlvin() throws Exception {
+
+		UpdateAllRecordsAndLinkValidationType updater = new UpdateAllRecordsAndLinkValidationType(
+				ALVIN_LOCAL_APPTOKEN_URL, ALVIN_LOCAL_BASE_URL);
+
+		updater.updateAllRecords();
+	}
+
+	@Test(enabled = false)
+	public void runDevSystemOne() throws Exception {
+
+		UpdateAllRecordsAndLinkValidationType updater = new UpdateAllRecordsAndLinkValidationType(
+				SYSTEMONE_DEV_APPTOKEN_URL, SYSTEMONE_DEV_BASE_URL);
+
+		updater.updateAllRecords();
+	}
+
+	@Test(enabled = false)
+	public void addMissingTextAndDefTextLocalSystemOne() throws Exception {
+
+		UpdateAllRecordsAndLinkValidationType updater = new UpdateAllRecordsAndLinkValidationType(
+				SYSTEMONE_LOCAL_APPTOKEN_URL, SYSTEMONE_LOCAL_BASE_URL);
+
+		updater.addMissingTextsAndDefTexts();
+	}
+
+	@Test(enabled = false)
+	public void addMissingTextAndDefTextDEVSystemOne() throws Exception {
+
+		UpdateAllRecordsAndLinkValidationType updater = new UpdateAllRecordsAndLinkValidationType(
+				SYSTEMONE_DEV_APPTOKEN_URL, SYSTEMONE_DEV_BASE_URL);
+
+		updater.addMissingTextsAndDefTexts();
 	}
 
 }
