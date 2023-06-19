387c8d37c2f6f50fd411e2d3ee8b2178005b603b
pere <pere@a716ea700564>
2023-06-19 15:02:05 +0000

    set tests to false

--- src/test/java/se/uu/ub/cora/change/SetFinalValueForValidationTypeTest.java
+++ src/test/java/se/uu/ub/cora/change/SetFinalValueForValidationTypeTest.java
@@ -22,7 +22,7 @@ public class SetFinalValueForValidationTypeTest {
 	private static final String DIVA_DEV_APPTOKEN_URL = "http://130.238.171.238:38182/apptokenverifier/rest/";
 	private static final String DIVA_DEV_BASE_URL = "http://130.238.171.238:38082/diva/rest/";
 
-	@Test(enabled = true)
+	@Test(enabled = false)
 	public void addFinalValueForValidationType() throws Exception {
 
 		SetFinalValueForValidationType updater = new SetFinalValueForValidationType(
