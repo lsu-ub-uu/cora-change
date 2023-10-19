fa686922938bd6abe2e43c2d9d5cc9f762b0a798
olovm <olov.mckie@ub.uu.se>
2023-10-19 22:17:41 +0000

    added script to change validationType for texts in diva

--- src/main/java/se/uu/ub/cora/change/ChangeTextsToDivaText.java
+++ src/main/java/se/uu/ub/cora/change/ChangeTextsToDivaText.java
@@ -1,13 +1,17 @@
 package se.uu.ub.cora.change;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.util.Map;
-import java.util.Optional;
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import se.uu.ub.cora.clientdata.ClientData;
 import se.uu.ub.cora.clientdata.ClientDataAtomic;
+import se.uu.ub.cora.clientdata.ClientDataChild;
+import se.uu.ub.cora.clientdata.ClientDataChildFilter;
 import se.uu.ub.cora.clientdata.ClientDataGroup;
 import se.uu.ub.cora.clientdata.ClientDataList;
 import se.uu.ub.cora.clientdata.ClientDataProvider;
@@ -18,15 +22,30 @@ import se.uu.ub.cora.javaclient.cora.DataClient;
 import se.uu.ub.cora.javaclient.cora.DataClientFactoryImp;
 
 public class ChangeTextsToDivaText {
+	public static final String ANSI_BOLD = "\033[0;1m";
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
 
-	private static final String VALIDATION_DEF_TEXT = "ValidationDefText";
-	private static final String ID = "id";
-	private static final String DATA_DIVIDER = "dataDivider";
 	private static final String RECORD_INFO = "recordInfo";
+	private static final String VALIDATION_TYPE = "validationType";
+	private static final int DEFAULT_THREAD_COUNT = 20;
+	private static final int DEFAULT_QUEUE_LENGTH = 20;
 	private DataClientFactoryImp dataClientFactory;
 	private DataClient dataClient;
 	private String apptokenUrl;
 	private String baseUrl;
+	private int totalDivaTexts = 0;
+	private int divaTextChangeNeeded = 0;
+	private int totalOtherTexts = 0;
+	private String totalNumberOfTypeInStorage;
+	private ExecutorService executorService;
 
 	public ChangeTextsToDivaText(String apptokenUrl, String baseUrl) {
 		this.apptokenUrl = apptokenUrl;
@@ -39,144 +58,116 @@ public class ChangeTextsToDivaText {
 	}
 
 	public void changeValidationTypeForDivaTexts() {
-		ClientDataList listOfTexts = dataClient.readList("text");
-		for (ClientData clientData : listOfTexts.getDataList()) {
-			ClientDataRecord textRecord = (ClientDataRecord) clientData;
+		// executorService = Executors.newFixedThreadPool(30);
+		executorService = new ThreadPoolExecutor(DEFAULT_THREAD_COUNT, DEFAULT_THREAD_COUNT, 2,
+				TimeUnit.MINUTES, new ArrayBlockingQueue<>(DEFAULT_QUEUE_LENGTH),
+				new ThreadPoolExecutor.CallerRunsPolicy());
 
-			ClientDataRecordGroup textRecordGroup = textRecord.getDataRecordGroup();
-			String id = textRecordGroup.getId();
-			if ("systemOne".equals(textRecordGroup.getDataDivider())) {
-				handleDivaText(textRecordGroup, id);
-			} else {
-				System.out.println("id: " + id);
-			}
-			//
-			// textRecordGroup.removeChildrenWithTypeAndName(ClientDataRecordLink.class,
-			// "validatesRecordType");
-			// ClientDataRecordLink newLink = ClientDataProvider
-			// .createRecordLinkUsingNameInDataAndTypeAndId("validatesRecordType",
-			// "recordType", id);
-			//
-			// textRecordGroup.addChild(newLink);
-			//
-			// dataClient.update("validationType", id, textRecordGroup);
+		ClientDataList listOfTexts = dataClient.readList("text");
+		totalNumberOfTypeInStorage = listOfTexts.getTotalNumberOfTypeInStorage();
+		for (ClientData clientData : listOfTexts.getDataList()) {
+			handleOneText(clientData);
 		}
+		systemOutPrintlnBoldGreen("Total texts: " + totalNumberOfTypeInStorage);
+		systemOutPrintlnBoldGreen("Total diva texts: " + totalDivaTexts);
+		systemOutPrintlnBoldGreen("Diva texts change: " + divaTextChangeNeeded);
+		systemOutPrintlnBoldYellow("Other texts: " + totalOtherTexts);
+		executorService.shutdown();
+	}
+
+	private void handleOneText(ClientData clientData) {
+		ClientDataRecord textRecord = (ClientDataRecord) clientData;
+
+		ClientDataRecordGroup textRecordGroup = textRecord.getDataRecordGroup();
+		String id = textRecordGroup.getId();
+		if ("diva".equals(textRecordGroup.getDataDivider())) {
+			totalDivaTexts++;
+			handleDivaText(textRecordGroup, id);
+		} else {
+			totalOtherTexts++;
+			System.out.println("id: " + id);
+		}
+		System.out.println(MessageFormat.format("Texts handled: {0} / {1}",
+				totalDivaTexts + totalOtherTexts, totalNumberOfTypeInStorage));
 	}
 
 	private void handleDivaText(ClientDataRecordGroup textRecordGroup, String id) {
 		systemOutPrintlnBoldGreen("id: " + id);
-		ClientDataGroup recordInfo = textRecordGroup.getFirstGroupWithNameInData("recordInfo");
+		ClientDataGroup recordInfo = textRecordGroup.getFirstGroupWithNameInData(RECORD_INFO);
 		ClientDataRecordLink validationType = recordInfo
-				.getFirstChildOfTypeAndName(ClientDataRecordLink.class, "validationType");
+				.getFirstChildOfTypeAndName(ClientDataRecordLink.class, VALIDATION_TYPE);
 		systemOutPrintlnBoldGreen(validationType.getLinkedRecordId());
-		if (!"textSystemOne".equals(validationType.getLinkedRecordId())) {
-			systemOutPrintlnBoldGreen(validationType.getLinkedRecordId());
-			ClientDataRecordLink newValidationType = ClientDataProvider
-					.createRecordLinkUsingNameInDataAndTypeAndId(validationType.getNameInData(),
-							validationType.getLinkedRecordType(), "textSystemOne");
-			textRecordGroup.removeChildrenWithTypeAndName(ClientDataRecordLink.class, "recordInfo");
-			textRecordGroup.addChild(newValidationType);
-			systemOutPrintlnBoldGreen("changed validationType... updating record with id: " + id);
-			dataClient.update("text", id, textRecordGroup);
-			systemOutPrintlnBoldGreen("...updated");
-
+		if (!"divaText".equals(validationType.getLinkedRecordId())) {
+			handleTextChange(textRecordGroup, id, recordInfo, validationType);
 		}
 	}
 
-	public void changeValidationTypesToAbstractLevel(String system) {
-		Optional<Map<String, String>> oMapFromDisk = readFromDisk(apptokenUrl, baseUrl, system);
-		if (oMapFromDisk.isPresent()) {
-			Map<String, String> mapFromDisk = oMapFromDisk.get();
-			ClientDataList listOfValidationTypes = dataClient.readList("validationType");
-			for (ClientData validationTypeData : listOfValidationTypes.getDataList()) {
-				changeValidationTypeToAbstractForOneRecord(mapFromDisk, validationTypeData);
-			}
-		} else {
-			System.err.println("Not possible to read map for system: " + system);
-		}
+	private void handleTextChange(ClientDataRecordGroup textRecordGroup, String id,
+			ClientDataGroup recordInfo, ClientDataRecordLink validationType) {
+		divaTextChangeNeeded++;
+		ClientDataRecordLink newValidationType = ClientDataProvider
+				.createRecordLinkUsingNameInDataAndTypeAndId(validationType.getNameInData(),
+						validationType.getLinkedRecordType(), "divaText");
+		recordInfo.removeChildrenWithTypeAndName(ClientDataRecordLink.class, VALIDATION_TYPE);
+		recordInfo.addChild(newValidationType);
+		systemOutPrintlnBoldYellow("changed validationType... updating record with id: " + id);
 
+		if (textRecordGroup.getAllChildrenWithNameInData("textPart").size() == 1) {
+			createAndAddNewEnglishTextPart(textRecordGroup, id);
+		}
+		ClientDataChildFilter filter = ClientDataProvider
+				.createDataChildFilterUsingChildNameInData("textPart");
+		filter.addAttributeUsingNameInDataAndPossibleValues("lang", Set.of("en"));
+		filter.addAttributeUsingNameInDataAndPossibleValues("type", Set.of("alternative"));
+		List<ClientDataChild> enParts = textRecordGroup.getAllChildrenMatchingFilter(filter);
+		for (ClientDataChild enPart : enParts) {
+			ClientDataGroup enGroup = (ClientDataGroup) enPart;
+			if (enGroup.getFirstAtomicValueWithNameInData("text").getBytes().length == 0) {
+				systemOutPrintlnBoldRed("EMPTY ENGLISH!!!!");
+				textRecordGroup.removeAllChildrenMatchingFilter(filter);
+				createAndAddNewEnglishTextPart(textRecordGroup, id);
+			}
+		}
+		if (!id.equals("returnText")
+				&& !id.equals("doctoralThesisContentTypeCollectionVarDefText")) {
+			Runnable runnableTask = () -> {
+				systemOutPrintlnBoldRed("START!!!!");
+				systemOutPrintlnBoldYellow("storing id: " + id);
+				dataClient.update("text", id, textRecordGroup);
+				systemOutPrintlnBoldRed("DONE!!!!");
+			};
+			executorService.execute(runnableTask);
+		}
+		systemOutPrintlnBoldGreen("...updated");
 	}
 
-	private void changeValidationTypeToAbstractForOneRecord(Map<String, String> mapFromDisk,
-			ClientData validationTypeData) {
-		ClientDataRecord validationTypeRecord = (ClientDataRecord) validationTypeData;
-		ClientDataRecordGroup validationTypeRecordGroup = validationTypeRecord.getDataRecordGroup();
+	private void createAndAddNewEnglishTextPart(ClientDataRecordGroup textRecordGroup, String id) {
+		String english = "CHANGE ME, my id is: " + id;
+		systemOutPrintlnBoldRed(english);
+		ClientDataGroup textPart = createNewEnglishTextPartWithString(english);
+		textRecordGroup.addChild(textPart);
+	}
 
-		ClientDataRecordLink validatesLink = validationTypeRecordGroup
-				.getFirstChildOfTypeAndName(ClientDataRecordLink.class, "validatesRecordType");
-		String currentValidatesId = validatesLink.getLinkedRecordId();
-		System.out.println("Checking: " + currentValidatesId);
-
-		if (mapFromDisk.containsKey(currentValidatesId)) {
-			String id = mapFromDisk.get(currentValidatesId);
-			if (!currentValidatesId.equals(id)) {
-				validationTypeRecordGroup.removeChildrenWithTypeAndName(ClientDataRecordLink.class,
-						"validatesRecordType");
-				ClientDataRecordLink newLink = ClientDataProvider
-						.createRecordLinkUsingNameInDataAndTypeAndId("validatesRecordType",
-								"recordType", id);
-				validationTypeRecordGroup.addChild(newLink);
-				dataClient.update("validationType", validationTypeRecordGroup.getId(),
-						validationTypeRecordGroup);
-				systemOutPrintlnBoldGreen("UpdatedvalidatesRecordTypeId: %s --> %s"
-						.formatted(currentValidatesId, id));
-			} else {
-				System.out.println("Skipping same in map: " + currentValidatesId);
-			}
-		} else {
-			System.err.println(
-					"Current validatesRecordTypeId not found in map: " + currentValidatesId);
-		}
+	private ClientDataGroup createNewEnglishTextPartWithString(String english) {
+		ClientDataGroup textPart = ClientDataProvider.createGroupUsingNameInData("textPart");
+		textPart.addAttributeByIdWithValue("type", "alternative");
+		textPart.addAttributeByIdWithValue("lang", "en");
+		ClientDataAtomic englishPart = ClientDataProvider
+				.createAtomicUsingNameInDataAndValue("text", english);
+		textPart.addChild(englishPart);
+		return textPart;
 	}
 
 	private void systemOutPrintlnBoldGreen(String string) {
-		System.out.println("\033[0;1m\u001B[32m" + string + "\u001B[0m");
+		System.out.println(ANSI_BOLD + ANSI_GREEN + string + ANSI_RESET);
 	}
 
-	private Optional<Map<String, String>> readFromDisk(String appTokenUrl, String baseUrl,
-			String system) {
-		String filePath = System.getProperty("user.home") + "/workspace/cora-change/impToParent"
-				+ system + ".ser";
-
-		Optional<Map<String, String>> readMapFromFile = readMapFromFile(filePath);
-		System.out.println(readMapFromFile);
-		return readMapFromFile;
+	private void systemOutPrintlnBoldRed(String string) {
+		System.out.println(ANSI_BOLD + ANSI_RED + string + ANSI_RESET);
 	}
 
-	public Optional<Map<String, String>> readMapFromFile(String filePath) {
-
-		try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))) {
-			Map<String, String> readMap = (Map<String, String>) ois.readObject();
-			return Optional.of(readMap);
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return Optional.empty();
+	private void systemOutPrintlnBoldYellow(String string) {
+		System.out.println(ANSI_BOLD + ANSI_YELLOW + string + ANSI_RESET);
 	}
 
-	public void changeAbstractRecordTypesToImplementing() {
-		ClientDataList listOfRecordTypes = dataClient.readList("recordType");
-		for (ClientData recordTypeData : listOfRecordTypes.getDataList()) {
-			ClientDataRecord recordTypeRecord = (ClientDataRecord) recordTypeData;
-			ClientDataRecordGroup recordTypeRecordGroup = recordTypeRecord.getDataRecordGroup();
-
-			ClientDataAtomic abstractAtomic = recordTypeRecordGroup
-					.getFirstChildOfTypeAndName(ClientDataAtomic.class, "abstract");
-			if ("true".equals(abstractAtomic.getValue())) {
-				recordTypeRecordGroup.removeChildrenWithTypeAndName(ClientDataAtomic.class,
-						"abstract");
-				ClientDataAtomic newAtomic = ClientDataProvider
-						.createAtomicUsingNameInDataAndValue("abstract", "false");
-
-				recordTypeRecordGroup.addChild(newAtomic);
-
-				// if (!"metadata".equals(recordTypeRecordGroup.getId())) {
-				systemOutPrintlnBoldGreen(
-						"changing to implementing for: " + recordTypeRecordGroup.getId());
-				dataClient.update("recordType", recordTypeRecordGroup.getId(),
-						recordTypeRecordGroup);
-				// }
-			}
-		}
-	}
 }
