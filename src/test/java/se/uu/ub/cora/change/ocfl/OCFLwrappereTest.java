3cf911695c4833dcd1d09450748bc1c157139824
olovm <olov.mckie@ub.uu.se>
2025-03-05 15:34:25 +0000

    some updates calling file as it should be on disk to make things easier..

--- src/test/java/se/uu/ub/cora/change/ocfl/OCFLwrappereTest.java
+++ src/test/java/se/uu/ub/cora/change/ocfl/OCFLwrappereTest.java
@@ -4,6 +4,7 @@ import static org.testng.Assert.assertEquals;
 
 import java.io.File;
 import java.nio.file.Paths;
+import java.text.MessageFormat;
 
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -26,29 +27,39 @@ public class OCFLwrappereTest {
 	}
 
 	@Test
-	public void testDirs() throws Exception {
+	public void testDirs() {
 		createDir("ocfl/ocfl-repo2");
 		createDir("ocfl/ocfl-work2");
 
 	}
 
 	@Test
-	public void testPutOneBinary() throws Exception {
+	public void testPutOneBinary() {
 		// createDir("ocfl/ocfl-repo");
 		// createDir("ocfl/ocfl-work");
 
-		var repoDir = Paths.get("ocfl/ocfl-repo"); // This directory contains the OCFL
-													// storage root.
-		var workDir = Paths.get("ocfl/ocfl-work"); // This directory is used to assemble OCFL
-													// versions.
-		// It cannot be within the OCFL storage root.
+		// This directory contains the OCFL storage root.
+		// var repoDir = Paths.get("ocfl/ocfl-repo");
+		var repoDir = Paths.get("/tmp/sharedArchive/systemOne");
+		// This directory is used to assemble OCFL versions. It cannot be within the OCFL storage
+		// root.
+		var workDir = Paths.get("ocfl/ocfl-work");
 
 		var repo = new OcflRepositoryBuilder().defaultLayoutConfig(new HashedNTupleLayoutConfig())
 				.storage(storage -> storage.fileSystem(repoDir)).workDir(workDir).build();
 
+		String ocflPathLayout = "info:fedora/{0}:{1}:{2}-master";
+		String objectIdExample = "1ee5d8f9093420df54b2f81b8253d873233e4a008ae3290ffded8f1ed0225242";
+		String objectId = generateObjectIdentifier("systemOne", "binary", "binary:001");
 		// repo.putObject(ObjectVersionId.head("o1"), Paths.get("ocfl/object-out-dir"),
-		repo.putObject(ObjectVersionId.head("o1"), Paths.get("ocfl/failedHard.png"),
-				new VersionInfo().setMessage("initial commit"), OcflOption.MOVE_SOURCE);
+
+		VersionInfo versionInfo = new VersionInfo();
+		versionInfo.setMessage("initial commit");
+		versionInfo.setUser("fedoraAdmin", "info:fedora/fedoraAdmin");
+
+		repo.putObject(ObjectVersionId.head(objectId),
+				Paths.get("ocfl/systemOne:binary:binary:001-master"), versionInfo,
+				OcflOption.MOVE_SOURCE);
 
 		// Contains object details and lazy-load resource handles
 		OcflObjectVersion objectVersion = repo.getObject(ObjectVersionId.version("o1", "v1"));
@@ -57,11 +68,13 @@ public class OCFLwrappereTest {
 
 	}
 
-	@Test
-	public void testName() throws Exception {
-		createDir("ocfl/ocfl-repo");
-		createDir("ocfl/ocfl-work");
+	private String generateObjectIdentifier(String dataDivider, String type, String id) {
+		String ocflPathLayout = "info:fedora/{0}:{1}:{2}-master";
+		return MessageFormat.format(ocflPathLayout, dataDivider, type, id);
+	}
 
+	@Test
+	public void testUpdate() {
 		var repoDir = Paths.get("ocfl/ocfl-repo"); // This directory contains the OCFL
 		// storage root.
 		var workDir = Paths.get("ocfl/ocfl-work"); // This directory is used to assemble OCFL
@@ -70,6 +83,8 @@ public class OCFLwrappereTest {
 
 		var repo = new OcflRepositoryBuilder().defaultLayoutConfig(new HashedNTupleLayoutConfig())
 				.storage(storage -> storage.fileSystem(repoDir)).workDir(workDir).build();
+
+		String ocflPathLayout = "info:fedora/{0}:{1}:{2}-master";
 
 		// repo.putObject(ObjectVersionId.head("o1"), Paths.get("ocfl/object-out-dir"),
 		repo.putObject(ObjectVersionId.head("o1"), Paths.get("ocfl/failed.png"),
