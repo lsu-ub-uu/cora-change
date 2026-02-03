0f667732aac4b0d889509cfe4164955a6d817e68
olovm <olov.mckie@ub.uu.se>
2026-02-03 07:20:50 +0000

    disabling test by default

--- src/test/java/se/uu/ub/cora/change/CreateManyRecordsAtOnceTest.java
+++ src/test/java/se/uu/ub/cora/change/CreateManyRecordsAtOnceTest.java
@@ -7,10 +7,10 @@ public class CreateManyRecordsAtOnceTest {
 	private static final String SYSTEMONE_LOCAL_BASE_URL = "http://localhost:8080/systemone/rest/";
 	private static final String SYSTEMONE_LOCAL_APPTOKEN_URL = "http://localhost:8180/login/rest/";
 
-	@Test(enabled = true)
+	@Test(enabled = false)
 	public void runCreateManyDemosAtOnce() {
-		CreateManyRecordsAtOnce creator = new CreateManyRecordsAtOnce(
-				SYSTEMONE_LOCAL_APPTOKEN_URL, SYSTEMONE_LOCAL_BASE_URL);
+		CreateManyRecordsAtOnce creator = new CreateManyRecordsAtOnce(SYSTEMONE_LOCAL_APPTOKEN_URL,
+				SYSTEMONE_LOCAL_BASE_URL);
 
 		creator.createExamples();
 	}
